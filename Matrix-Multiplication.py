e6b52bfec7caef9a11d79d82d1999d5d56f46ecb
Danial Soleimany <102956315+DanialSoleimany@users.noreply.github.com>
2023-05-12 21:04:22 +0330

    Matrix-Multiplication.py

--- Matrix-Multiplication.py
+++ Matrix-Multiplication.py
@@ -3,7 +3,8 @@ import numpy as np
 def dot(mat1, mat2):
     return np.dot(mat1, mat2)
 
-mat1 = [[1, 0], [0, 1]]
-mat2 = [[4, 1], [2, 2]]
+A = [[1, 0], [0, 1]]
+B = [[4, 1], [2, 2]]
+AB = dot(A, B)
 
-print(dot(mat1, mat2))
+print(AB)
